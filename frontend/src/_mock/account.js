9643b24ed3e3620be9d1f770db0ac2b50a4ae857
Neorace22 <siddarthsajeev2002@gmail.com>
2023-05-30 11:20:07 +0530

    showing name of user instead of default name

--- frontend/src/_mock/account.js
+++ frontend/src/_mock/account.js
@@ -1,10 +1,20 @@
 // ----------------------------------------------------------------------
+import { useContext } from 'react';
+import { DataContext } from '../DataContext';
 
 
-const account = {
-  displayName: 'Sumesh Divakaran',
-  email: 'sdivakaran@cet.ac.in',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
-};
+function Account() {
 
-export default account;
+  const { hellodata } = useContext(DataContext);
+  const {name} = hellodata;
+  const acc = {
+    displayName: `${name.name.firstName} ${name.name.lastName}`,
+    email: 'sdivakaran@cet.ac.in',
+    photoURL: '/assets/images/avatars/avatar_default.jpg',
+  };
+
+  return acc;
+}
+
+
+export default Account;
