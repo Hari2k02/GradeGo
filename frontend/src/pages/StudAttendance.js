7a91977bc08859f6629d46785b314099218410f7
Neorace22 <siddarthsajeev2002@gmail.com>
2023-05-30 12:24:16 +0530

    Update StudAttendance.js

--- frontend/src/pages/StudAttendance.js
+++ frontend/src/pages/StudAttendance.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useTheme } from '@mui/material/styles';
-import { Grid, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import { Grid, Select, MenuItem, InputLabel, FormControl, CircularProgress, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 import { DataContext } from '../DataContext';
 
 import {
@@ -8,10 +8,12 @@ import {
 } from '../sections/@dashboard/app';
 
 export default function StudAttendance() {
+  const [loading, setLoading] = useState(false);
   const { hellodata } = useContext(DataContext);
   const theme = useTheme();
   const [selectedCourse, setSelectedCourse] = useState('');
   const [attendanceData, setAttendanceData] = useState({});
+  const [isNoDataPopupOpen, setIsNoDataPopupOpen] = useState(false);
 
   // Handle course selection
   const handleCourseChange = (event) => {
@@ -26,6 +28,7 @@ export default function StudAttendance() {
   }, [selectedCourse, hellodata]);
 
   const fetchAttendanceData = async () => {
+    setLoading(true);
     try {
       const response = await fetch('https://gradego-rtib.onrender.com/attendance/student', {
         method: 'POST',
@@ -41,15 +44,25 @@ export default function StudAttendance() {
       if (response.ok) {
         const data = await response.json();
         setAttendanceData(data);
-      } else {
+        setLoading(false);
+      } 
+      
+      else {
         // Handle error response
         console.log('Error:', response.status);
-        
+        setAttendanceData({});
+        setLoading(false);
+        setIsNoDataPopupOpen(true);
+
       }
     } catch (error) {
       // Handle fetch error
       console.log('Fetch Error:', error);
     }
+  };
+
+  const handleNoDataPopupClose = () => {
+    setIsNoDataPopupOpen(false);
   };
 
   return (
@@ -75,17 +88,41 @@ export default function StudAttendance() {
             ))}
           </Select>
         </FormControl>
-        <AppCurrentVisits
-          title="Students Attendance"
-          chartData={[
-            { label: 'Present', value: attendanceData?.presentCount || 0 },
-            { label: 'Absent', value: attendanceData?.totalAttendanceLength ? attendanceData.totalAttendanceLength - attendanceData.presentCount : 0 },
-          ]}
-          chartColors={[
-            theme.palette.success.main,
-            theme.palette.error.main,
-          ]}
-        />
+        {loading ? (
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '300px', // Set a minimum height for the container
+          }}>
+            <CircularProgress />
+          </div>
+        ) : (
+          <AppCurrentVisits
+            title="Students Attendance"
+            chartData={[
+              { label: 'Present', value: attendanceData?.presentCount || 0 },
+              { label: 'Absent', value: attendanceData?.totalAttendanceLength ? attendanceData.totalAttendanceLength - attendanceData.presentCount : 0 },
+            ]}
+            chartColors={[
+              theme.palette.success.main,
+              theme.palette.error.main,
+            ]}
+          />
+        )}
+
+        {/* No data popup */}
+        <Dialog open={isNoDataPopupOpen} onClose={handleNoDataPopupClose}>
+          <DialogTitle>No Data Available</DialogTitle>
+          <DialogContent>
+            <p>No attendance data is available for the selected course.</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleNoDataPopupClose} color="primary" autoFocus>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     </>
   );
