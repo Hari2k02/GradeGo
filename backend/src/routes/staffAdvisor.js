9a75bdc15796a0075a0951a3bd75eaab22b0952e
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-31 14:21:55 +0530

    Update staffAdvisor.js

--- backend/src/routes/staffAdvisor.js
+++ backend/src/routes/staffAdvisor.js
@@ -107,13 +107,13 @@ router.post('/facdashboard/semesterCourses', async (req, res) => {
 router.post('/facdashboard/studentAttendance', async (req, res) => {
   try {
     const { _id, courseCode } = req.body;
-    // find the semester an batch from the _id
+    // find the semester and batch from the _id
     const semesterAndBatch = await StaffAdvisor.findOne({ _id: _id }, { semesterHandled: 1, batchHandled: 1 });
     // now find the students with this semester and batch
     const semester = semesterAndBatch.semesterHandled;
     const batch = semesterAndBatch.batchHandled;
     const students = await Students.find({ currentSemester: semester, batch: batch }, { _id: 1 });
-    // console.log(students);
+
     let studentsList = [];
     for (let i = 0; i < students.length; ++i) {
       const studentId = await StudentCourses.findOne({ _id: students[i]._id, 'coursesEnrolled.semesterCourses.courseCode': courseCode }, { _id: 1 });
@@ -121,47 +121,48 @@ router.post('/facdashboard/studentAttendance', async (req, res) => {
       if (studentNameList) {
         studentsList.push(studentNameList);
       }
-      // if (studentId) {
-      //   studentsList.push(studentId);
-      // }
     }
 
-
-    let ktuIds = [];
-    for (let i = 0; i < studentsList.length; i++) {
-      ktuIds.push(studentsList[i]._id);
-    }
+    let ktuIds = studentsList.map(student => student._id); // Simplified array mapping
 
     const results = await InternalMarks.aggregate([
       { $unwind: '$courseAssessmentTheory' },
       { $match: { _id: { $in: ktuIds }, 'courseAssessmentTheory.courseCode': courseCode } },
       { $unwind: '$courseAssessmentTheory.attendance' },
       {
-        $match: {
-          'courseAssessmentTheory.attendance.isPresent': true
-        }
-      },
-      {
         $group: {
           _id: '$_id',
-          presentDays: { $addToSet: '$courseAssessmentTheory.attendance.date' },
+          presentDays: {
+            $sum: {
+              $cond: [
+                { $eq: ['$courseAssessmentTheory.attendance.isPresent', true] },
+                1,
+                0
+              ]
+            }
+          },
           totalDays: { $sum: 1 }
         }
       }
     ]).exec();
+
     let output = [];
-    console.log(results);
     // Log the attendance statistics
     for (let i = 0; i < results.length; ++i) {
       const studentName = await Students.findOne({ _id: results[i]._id }, { name: 1 });
-      output.push({ '_id': results[i]._id, 'name': studentName, 'presentDays': results[i].presentDays.length, 'totalDays': results[i].totalDays });
+      output.push({
+        '_id': results[i]._id,
+        'name': studentName,
+        'presentDays': results[i].presentDays,
+        'totalDays': results[i].totalDays
+      });
     }
     return res.json(output);
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Internal server error' });
   }
 });
+
 
 module.exports = router;
