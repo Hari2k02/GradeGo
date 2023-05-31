1759af51dafe019c1c5d891035790d14d23e5d52
Neorace22 <siddarthsajeev2002@gmail.com>
2023-05-31 14:52:45 +0530

    Update feature-card.js

--- frontend/src/components/landingpage/feature-card.js
+++ frontend/src/components/landingpage/feature-card.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
 import PropTypes from 'prop-types'
-
+import { fadeIn, staggerContainer } from '../../utils/motion'
+import { motion } from 'framer-motion'
 import './feature-card.css'
 
 const FeatureCard = (props) => {
   return (
-    <div className="feature-card-container">
-      <img
-        alt={props.image_alt}
-        src={props.image_src}
-        className="feature-card-image"
-      />
-      <h5 className="feature-card-text HeadingThree">{props.new_prop}</h5>
-      <span className="feature-card-text1">{props.text}</span>
-    </div>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="feature-card-container">
+        <motion.img
+        variants={fadeIn("up","tween",0.3,1)}
+          alt={props.image_alt}
+          src={props.image_src}
+          className="feature-card-image"
+        />
+        <motion.h5 variants={fadeIn("up","tween",0.3,1)} className="feature-card-text HeadingThree">{props.new_prop}</motion.h5>
+        <motion.span variants={fadeIn("up","tween",0.3,1)} className="feature-card-text1">{props.text}</motion.span>
+    </motion.div>
   )
 }
 
