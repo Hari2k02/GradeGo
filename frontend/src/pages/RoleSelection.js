308247f3d0fc5449ef09536906b9705ff5c3d419
Neorace22 <siddarthsajeev2002@gmail.com>
2023-06-17 17:33:27 +0530

    added animation to role selection

--- frontend/src/pages/RoleSelection.js
+++ frontend/src/pages/RoleSelection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { Card, CardActionArea, CardMedia, Typography } from '@mui/material';
+import { motion } from 'framer-motion'
+import { staggerContainer, fadeIn, slideIn } from '../utils/motion'
 
 const RootContainer = styled('div')({
     display: 'flex',
@@ -57,42 +59,56 @@ const RoleSelectionPage = () => {
     };
 
     return (
-        <RootContainer>
-            <HeadingTypography variant="h2" component="h1">
-                Role Selection
-            </HeadingTypography>
-            <CardContainer>
-                <CustomCard onClick={() => handleRoleSelection('tutor')}>
-                    <CardActionArea>
-                        <CardMedia
-                            component="img"
-                            alt="Tutor"
-                            height="300"
-                            image="/assets/images/avatars/teacher.jpg"
-                        />
-                        <RoleText variant="h5" component="h2">
-                            Tutor
-                        </RoleText>
-                    </CardActionArea>
-                </CustomCard>
-                <CustomCard onClick={() => handleRoleSelection('staff-advisor')}>
-                    <CardActionArea>
-                        <CardMedia
-                            component="img"
-                            alt="Staff Advisor"
-                            height="300"
-                            image="/assets/images/avatars/advisor.jpg"
-                        />
-                        <RoleText variant="h5" component="h2">
-                            Staff Advisor
-                        </RoleText>
-                    </CardActionArea>
-                </CustomCard>
-            </CardContainer>
-            <PromptTypography variant="h3" component="h1">
-                Please select a role to continue 
-            </PromptTypography>
-        </RootContainer>
+        <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}>
+            <RootContainer>
+                <motion.div variants={slideIn("up", "tween", 0, 1)}>
+                <HeadingTypography variant="h2" component="h1">
+                    Role Selection
+                </HeadingTypography>
+                </motion.div>
+                <CardContainer>
+                    <motion.div variants={fadeIn("right", "tween", 0.3, 1)}>
+                        <CustomCard onClick={() => handleRoleSelection('tutor')}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Tutor"
+                                    height="300"
+                                    image="/assets/images/avatars/teacher.jpg"
+                                />
+                                <RoleText variant="h5" component="h2">
+                                    Tutor
+                                </RoleText>
+                            </CardActionArea>
+                        </CustomCard>
+                    </motion.div>
+                    <motion.div variants={fadeIn("left", "tween", 0.3, 1)}>
+                    <CustomCard onClick={() => handleRoleSelection('staff-advisor')}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Staff Advisor"
+                                height="300"
+                                image="/assets/images/avatars/advisor.jpg"
+                            />
+                            <RoleText variant="h5" component="h2">
+                                Staff Advisor
+                            </RoleText>
+                        </CardActionArea>
+                    </CustomCard>
+                    </motion.div>
+                </CardContainer>
+                <motion.div variants={slideIn("up", "tween", 0, 1)}>
+                <PromptTypography variant="h3" component="h1">
+                    Please select a role to continue
+                </PromptTypography>
+                </motion.div>
+            </RootContainer>
+        </motion.div>
     );
 };
 
