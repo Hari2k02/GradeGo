5959aa50c3d9dcaffc5ffe1ebf4d2f78d19f86f9
Hari2k02 <harivellikkunnam@gmail.com>
2023-07-05 01:17:48 +0530

    modification in connect

--- backend/src/connect/mongoose.js
+++ backend/src/connect/mongoose.js
@@ -1,7 +1,6 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const mongoose = require('mongoose');
-const db = 'mongodb+srv://StudentTracker:EBJempYE3pbx4xSQ@cluster0.i8kgzaf.mongodb.net/node_system';
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, family:4 })
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, family:4 })
   .then(result => console.log('connected to db'))
   .catch(err => console.log(err));
