27431d4ce882df1cc451048fc8b6627597d4792b
RishinR <rishinrnew2020@gmail.com>
2023-09-24 21:55:12 +0530

    added api to add new faculty by admin

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -81,4 +81,15 @@ router.post('/admin/facultyCourseAssignment', async (req, res) => {
   }
 });
 
+router.post('/admin/newFaculty', async (req, res) => {
+  try {
+    const input = req.body;    
+    const out = await Faculty.create(input);
+    return res.json({ status: 'ok' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 module.exports = router;
